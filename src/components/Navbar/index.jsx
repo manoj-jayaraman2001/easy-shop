c31098c70ef806bc5e2a58832cc7b4e83d95bcd5
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-09-18 22:08:07 +0530

    layout completed

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div>
+            <div className="wrapper">
+                <div className="left"></div>
+                <div className="center"></div>
+                <div className="right"></div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
